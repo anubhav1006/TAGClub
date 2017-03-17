b2e7e2f61482461d670fc1ed42982644f8d8f710
Anubhav Jha <aj1006.aj@gmail.com>
2017-03-17 10:23:46 +0530

    Add Comment line after changing.

--- app/src/main/java/in/tagclub/tagclub/MainActivity.java
+++ app/src/main/java/in/tagclub/tagclub/MainActivity.java
@@ -3,7 +3,11 @@ package in.tagclub.tagclub;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//Add a comment line and your name whereever you make changes.
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
